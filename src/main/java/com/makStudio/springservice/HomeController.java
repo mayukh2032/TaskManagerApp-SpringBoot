60325fb921b34096ff948c3f9bc43f689bdc51b2
Mayukh Bhaskar <mayukh.bhaskar@gmail.com>
2017-07-21 18:09:11 +0530

    M.Bhaskar initial commit

---
+++ src/main/java/com/makStudio/springservice/HomeController.java
@@ -0,0 +1,11 @@
+package com.makStudio.springservice;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+ @RequestMapping("/home")
+ public String home() {
+	 System.out.println("inside HomeController");
+  return "index";
+ }
+}
